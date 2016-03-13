b78cd7e8932ef83330f3d3dcc0aeeb1a8daf7d8c
RikkeBarsten <rikkebarsten@gmail.com>
2016-03-13 07:35:15 +0100

    Initial commit

---
+++ VP_kort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_kort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
