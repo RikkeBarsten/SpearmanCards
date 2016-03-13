ad38bb5d5c46d49513e334135363c1bb1b0d1ad4
RikkeBarsten <rikkebarsten@gmail.com>
2016-03-13 07:51:06 +0100

    Add card class

---
+++ VP_kort/Cards.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP_kort
+{
+    enum Suit {Diamonds, Hearts, Clubs, Spades};
+
+    class Cards
+    {
+        public Suit CardSuit { get; set; }
+        public int Value { get; set; }
+    }
+}
